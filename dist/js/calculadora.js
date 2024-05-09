25102e74ba25afcfece6cf0d8f57baac79b02f11
zeneke <zeneke@vindrogames.com>
2024-05-09 23:29:57 +0200

    added functions to add numbers, and at least ADD and EQUALS

--- dist/js/calculadora.js
+++ dist/js/calculadora.js
@@ -5,6 +5,11 @@ const championsBtn = $('#champions');
 const equalsBtn = $('#equals');
 const audioBtn = $('.btn-audio');
 
+let promptNumber = 0;
+let firstOperand = 0;
+let secondOperand = 0;
+let operation = 'add'
+
 championsBtn.on('click', (e) => {
 
   if (e.target.classList.contains('play')) {
@@ -25,6 +30,49 @@ audioBtn.on('click', () => {
   equalsBtn.toggleClass('play');
   audioBtn.toggleClass('off');
 });
+
+function pushNumber(number)
+{
+  let result = $('#result').html();
+  if (promptNumber == 0)
+  {
+    promptNumber = number;
+    $('#result').html(promptNumber)
+  }
+  else
+  {
+    length = result.length
+    result = result + number
+    $('#result').html(result)
+  }
+}
+
+function addClicked()
+{
+  firstOperand = $('#result').html();
+  $('#result').html('0');
+  promptNumber = 0;  
+  operation = 'add'
+}
+
+function equalClicked()
+{
+  let secondOperand = $('#result').html();  
+  if (operation == 'add')
+  {    
+    let a = parseInt(firstOperand)
+    let b = parseInt(secondOperand)
+    console.log(parseInt(firstOperand));
+    console.log(parseInt(secondOperand));
+    let res = a + b
+    console.log(res);
+    $('#result').html(res);
+  }
+  firstOperand = 0;
+  secondOperand = 0;
+  promptNumber = 0;
+
+}
 
 
 
